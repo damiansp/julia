fb23e09a93c21a370196b088b4aa9f2d135a45c4
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-03 13:38:33 -0400

    +basics, types

---
+++ gettingStarted/02_variablesTypesOperations/01_basics.jl
@@ -0,0 +1,18 @@
+y = 7
+println(typeof(y))
+
+# Calculate the gravitational acceleraton
+G = 6.67e-11 # grav constant
+mass_earth = 5.98e24 # kg
+radius_earth = 6378100 # m
+gravitational_acceleration = G * mass_earth / radius_earth^2 # m/s^2
+println(gravitational_acceleration)
+
+#= 
+Multiline 
+comments in 
+Julia 
+=#
+
+
+
