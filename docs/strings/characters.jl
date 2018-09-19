eb296df3ca81d11814c99ee96f01d510c0642d05
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-18 20:18:10 -0400

    +char

---
+++ docs/strings/characters.jl
@@ -0,0 +1,10 @@
+x = 'x'
+println(typeof(x))      # Char
+println(Int(x))         # 120
+println(typeof(Int(x))) # Int64
+
+println(Char(120)) # x
+println('\u0')     # (blank)
+println('\u78')    # x
+println('\u2200')  # ∀
+
