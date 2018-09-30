6fdf3a38bb999ea3bd23a07ce2cec6225cdf8b99
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-30 13:45:25 -0400

    1st class++

--- gettingStarted/03_functions/first_class_and_closure.jl
+++ gettingStarted/03_functions/first_class_and_closure.jl
@@ -19,3 +19,45 @@ end
 
 f = x -> 2x^2 + 30x + 9
 println(numerical_derivative(f, 1, 0.0001))
+
+
+# Return funcs
+function derivative(f)
+    return function(x)
+        # select small value for h to approx deriv
+        h = x == 0 ? sqrt(eps(Float64)) : sqrt(eps(Float64)) * x
+        xph = x + h
+        dx = xph - x # why? dx = h
+        f1 = f(xph) # eval f at x + h
+        f0 = f(x)   # ''   '''' x
+        return (f1 - f0) / dx
+    end
+end
+
+
+function counter()
+    n = 0
+    () -> n += 1, () -> n = 0
+end
+
+(add_one, reset) = counter()
+println(add_one) # 1
+println(add_one) # 2
+println(add_one) # 3
+println(reset)   # 0
+
+
+# Currying
+function add(x)
+    return function f(y)
+        return x + y
+    end
+end
+
+println(add(1)(2))
+
+# can be written more succinctly as
+add(x) = f(y) = x + y # or
+add(x) = y -> x + y
+
+println(add(7)(5))
