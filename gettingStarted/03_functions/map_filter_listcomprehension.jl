2557b97b19a54e6e4122d4086b996e505250bc2a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-04 08:35:25 -0700

    +generic, map_filter++

--- gettingStarted/03_functions/map_filter_listcomprehension.jl
+++ gettingStarted/03_functions/map_filter_listcomprehension.jl
@@ -22,3 +22,20 @@ r2 = map(-3:3) do x
     end
 end
 println(r2)
+
+
+println(filter(n -> iseven(n), 1:10)) # [2, 4, 6, 8, 10]
+
+
+# list comprehensions
+arr = Float64[x^2 for x in 1:4]
+println(arr) # [1., 4., 9., 16.]
+cubes = [x^3 for x in 1:5]
+mat1 = [x + y for x in 1:2, y in 1:3]
+println(mat1) # [2 3 4; 3 4 5]
+table10 = [x * y for x=1:10, y=1:10]
+println(table10) # 10 x 10 mult table
+arrany = Any[i * 2 for i in 1:5]
+println(arrany) # Any[2, 4, 6, 8, 10]
+
+
