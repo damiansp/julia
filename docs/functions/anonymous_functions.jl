480e303a1190d0ee489a21b65b108bb87d75a18f
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-04 23:58:02 -0400

    +anon, opers are funcs, spec ops

---
+++ docs/functions/anonymous_functions.jl
@@ -0,0 +1,9 @@
+x -> x^2 + 2x - 1
+
+function(x)
+    x^2 + 2x - 1
+end
+
+
+println(map(round, [1.2, 3.5, 1.7]))
+println(map(x -> x^2 + 2x - 1, [1, 3, -1]))
---
+++ docs/functions/operators_are_functions.jl
@@ -0,0 +1,9 @@
+println(1 + 2 + 3)
+println(+(1, 2, 3))
+
+f = +;
+println(f(1, 2, 3))
+
+
+    
+        
