053e7a6f646626c37dc35dcf0d697524948994f4
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-28 19:54:33 -0400

    matrices++

--- gettingStarted/05_collectionTypes/matrices.jl
+++ gettingStarted/05_collectionTypes/matrices.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+
 col_vec = [1, 2, 3]
 col_vec2 = [1; 2; 3]
 row_vec = [1 2 3]
@@ -8,4 +10,26 @@ println(col_vec2)
 println(row_vec)
 println(matrix)
 
+m = rand(3, 5)
+println(m)
+println(ndims(m)) # 2
+println(size(m))  # (3, 5)
+nrows = size(m, 1) # 3
+ncols = size(m, 2) # 5
+println(nrows, ", ", ncols) # 3, 5
+nrows, ncols = size(m)
+println(nrows, ", ", ncols) # 3, 5
+println(length(m))    # 15
 
+i3 = 1 * Diagonal(I, 3)
+println(i3)
+
+i3 = [1 0 0; 0 1 0; 0 0 1]
+println(i3[:, 2])
+println(i3[2, :])
+println(i3[2:end, 2:end])
+
+i3[2, :] = [3 3 3]
+println(i3)
+i3[2:end, 2:end] = [5 7; 9 11]
+println(i3)
