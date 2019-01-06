76b4b307fffe79e2c174c531f9261f6f4aebde76
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-06 16:20:36 -0500

    add to 1 types

--- julia1.0/2_types/1_types.jl
+++ julia1.0/2_types/1_types.jl
@@ -3,3 +3,16 @@ x = "hello"
 
 # (2 + 3)::String # TypeError ... expected String, got Int64
 
+
+# Integers
+# type Int defaults to Int64 on 64-bit architecture
+# Available range is Int8-128
+a = typemax(Int)
+println(a)
+println(a + 1)
+
+
+# Floats (Float16-64)
+println(2.5f2) # 250
+println(1/0, -1/0) # Inf, -Inf
+println(0/0, Inf - Inf) # NaN, NaN
