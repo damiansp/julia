8b95f0c266e1e1e0b8235387511d3a1f7fb70a8c
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-20 01:47:54 -0500

    end generic functions

--- julia1.0/3_functions/generic_functions_multiple_dispatch.jl
+++ julia1.0/3_functions/generic_functions_multiple_dispatch.jl
@@ -4,3 +4,7 @@ f(n::Number, m) = "n is a number"
 f(n, m::Number) = "m is a number"
 f(n::Integer, m::Integer) = "n and m are both ints"
 
+println(f(1.5, 2))
+println(f(1, "bar"))
+println(f(1, 2))
+println(f("foo", [1, 2]))
