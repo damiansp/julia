a04e0e192c843ccf9f4f158d617ae524481dc8dc
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-30 00:39:23 -0500

    iris data exploration

--- programmingProjects/01_firstApp/iris.jl
+++ programmingProjects/01_firstApp/iris.jl
@@ -1,4 +1,5 @@
 using RDatasets
+using Statistics
 
 
 iris = dataset("datasets", "iris")
@@ -6,6 +7,24 @@ println("Data fields in iris:")
 println(names(iris))
 println("Iris size: ", size(iris))
 println("Head:")
-println(head(iris))
+println(first(iris, 5))
 println(by(iris, :Species, nrow))
+println(describe(iris))
+println(describe(iris, stats=[:std, :q25, :q75, :first, :last]))
+println(describe(iris, stats=:all))
+println(mean(iris[:SepalLength]))
+println(median(iris[:SepalLength]))
+println(std(iris[:SepalLength]))
 
+for x in names(iris)[1:end - 1]
+  for y in names(iris)[1:end - 1]
+    println("$x\t$y\t$(cor(iris[x], iris[y]))")
+  end
+  println("------------------------------------")
+end
+
+
+          
+
+
+        
