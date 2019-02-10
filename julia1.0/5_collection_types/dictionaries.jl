8f475fc8e8e8465541dfd1cf0c8c68afed6dbaec
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-02-10 16:32:58 -0500

    fin dicts and add sets

--- julia1.0/5_collection_types/dictionaries.jl
+++ julia1.0/5_collection_types/dictionaries.jl
@@ -14,10 +14,38 @@ println(haskey(d3, :Z)) # false
 println(haskey(d3, :B)) # true
 d3[:A] = 150
 d3[:C] = 300
-prinlnt(length(d3)) # 3
+println(length(d3)) # 3
 
 d4 = Dict() # empty, type=Any
 
 d5 = Dict{Float64, Int64}()
 
 delete!(d3, :B)
+
+
+# Looping
+for k in keys(d3)
+  println(k)
+end
+
+for v in values(d3)
+  println(v)
+end
+
+people = ["JS Bach", "Woody Allen", "Barack Obama"]
+yobs   = [     1685,          1935,           1961]
+d5 = Dict(zip(people, yobs))
+
+for (k, v) in d5
+  println("$k was born in $v")
+end
+
+for p in d5
+  println("$(p[1]) was born in $(p[2])")
+end
+
+arr_key = [key for (key, val) in d5] # same as:
+arr_key2 = collect(keys(d5))
+arr_val = [val for (key, val) in d5] # same as:
+arr_val2 = collect(values(d5))
+
