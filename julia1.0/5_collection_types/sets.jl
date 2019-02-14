89c8afd1824140ff3ca82177513224cba9e6da9b
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-02-14 11:59:19 -0500

    fin sets

--- julia1.0/5_collection_types/sets.jl
+++ julia1.0/5_collection_types/sets.jl
@@ -8,3 +8,7 @@ println(union(s1, s2)) # 11, 25, 3.14
 println(intersect(s1, s2)) # 25
 println(setdiff(s1, s))    # 11
 println(issubset(s1, s2))  # false
+
+push!(s1, 32)
+println(s1)
+println(in(32, s1))
