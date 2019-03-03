0323fc638eb40680ada09e2a61dbe695011ce6db
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-03 13:38:36 -0500

    finish inner constructors, init type unions

---
+++ julia1.0/6_types_methods_modules/type_unions.jl
@@ -0,0 +1,13 @@
+mutable struct Point
+  x::Float64
+  y::Float64
+end
+
+
+mutable struct Vector2D
+  x::Float64
+  y::Float64
+end
+
+p = Point(2, 5)
+v = Vector2D(3, 2)
--- julia1.0/6_types_methods_modules/types_collections_inner_constructors.jl
+++ julia1.0/6_types_methods_modules/types_collections_inner_constructors.jl
@@ -1,1 +1,28 @@
-# ph
+mutable struct Person
+  first_name::String
+  last_name::String
+  sex::Char
+  age::Float64
+  children::Array{String, 1}
+end
+
+p1 = Person("Alan", "Bates", 'M', 45.5, ["Jeff", "Stephen"])
+people = Person[]
+push!(people, p1)
+push!(people, Person("Julia", "Smith", 'F', 27, ["Viral"]))
+show(people)
+
+#full_name(p::Person) = "$(p.first_name) $(p.last_name)"
+full_name(p::Person) = string(p.first_name, " ", p.last_name)
+
+
+mutable struct Family
+  name::String
+  members::Array{String, 1}
+  big::Bool
+  Family(name::String) = new(name, String[], false)
+  Family(name::String, members) = new(name, members, length(members) > 4)
+end
+
+fam = Family("Bates-Smith", ["Alan", "Julia", "Stephen", "Jeff", "Viral"])
+show(fam)
