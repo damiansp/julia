82974c977ff97fa32ceeae9a008c8fe9fa3186c7
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-20 22:25:02 -0400

    add dictionaries; init http_response

--- programmingProjects/01_firstApp/dictionaries.jl
+++ programmingProjects/01_firstApp/dictionaries.jl
@@ -27,3 +27,22 @@ println(Dict(p1, p2))
 println(Dict([p1, p2]))
 
 println(Dict([x => Dates.dayname(x) for x = (1:7)]))
+
+d = Dict(:foo => 1, :bar => 2)
+println(d[:bar]) # 2
+#println(d[:fu])  # KeyError
+println(haskey(d, :fu)) # false
+println(get(d, :fu, 0))  # 0
+
+get!(d, :fu, 100)
+print(d) # ...:fu => 100
+println(haskey(d, :fu)) # true
+
+delete!(d, :fu)
+println(keys(d)) # :bar, :foo (KeySet)
+println(values(d)) # 2, 1
+println(collect(keys(d))) # array
+
+d2 = Dict(:fu => 3)
+d3 = merge(d, d2) # last collection overwrites previous if same key
+println(merge(d3, Dict(:fu => 10))) # :fu => 10
---
+++ programmingProjects/01_firstApp/http_response.jl
@@ -0,0 +1,1 @@
+headers = Dict(resp.headers)
