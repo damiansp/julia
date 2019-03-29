60beb08df055678a0dc4f8d2d74a0a03e68a1e7e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-29 07:41:14 -0700

    add column pull

--- julia1.0/8_io_networking_and_parallelization/csv.jl
+++ julia1.0/8_io_networking_and_parallelization/csv.jl
@@ -15,3 +15,10 @@ row2 = body[2, :]
 println(row2)
 col3 = body[:, 3]
 println(col3)
+
+z = [body[:, 1] body[:, 3]] # includes headers
+println(z)
+z = [body[2:end, 1] body[2:end, 3]] # exclude headers
+println(z)
+
+
