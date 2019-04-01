2a18a7bb63dc5478315b75ecf6b9b34d9d98a24e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-31 21:48:11 -0400

    add to functions

--- programmingProjects/01_firstApp/functions.jl
+++ programmingProjects/01_firstApp/functions.jl
@@ -14,3 +14,30 @@ function fake_add(x, y)
 end
 
 println(fake_add(3, 5))
+
+
+# Returning "multiple" values
+function mas_y_menos(x, y)
+  x + y, x - y
+end
+
+a, b = mas_y_menos(10, 5)
+println(a) # 15
+println(b) # 5
+
+function more_n_less(x=100, y=10)
+  x + y, x - y
+end
+
+println(more_n_less()) # 110, 90
+
+
+# Keyword args
+#function thermal_confort(
+#        temperature, humidity; scale=:celsius, age=35, health_risk=age / 100))
+#  ...
+#end
+
+# Invoke
+#thermal_confort(27, 56, age=72, scale=:fahrenheit)
+
