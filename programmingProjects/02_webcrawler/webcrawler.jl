46767fc4c949b81a5a51882a2cfebd16fc6e6600
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-06 00:40:33 -0400

    add to web crawler: parse links from dom

--- programmingProjects/02_webcrawler/webcrawler.jl
+++ programmingProjects/02_webcrawler/webcrawler.jl
@@ -7,11 +7,33 @@ const LINKS = String[] # same as Array{String, 1}()
 
 
 function fetch_page(url)
-    response = HTTP.get(url)
-    if (response.status == 200
-        && parse(Int, Dict(repsonse.headers)["Content-Length"]) > 0)
-    else
-        ""
-    end
+  response = HTTP.get(url)
+  if (response.status == 200
+      && parse(Int, Dict(response.headers)["Content-Length"]) > 0)
+    String(response.body)
+  else
+    ""
+  end
 end
 
+
+function extract_links(elem)
+  if (isa(elem, HTMLElement)
+      && tag(elem) == :a
+      && in("href", collect(keys(attrs(elem)))))
+    url = getattr(elem, "href")
+    startswith(url, "/wiki/") && push!(LINKS, url)
+  end
+  for child in children(elem)
+    extract_links(child)
+  end
+end
+
+
+content = fetch_page(PAGE_URL)
+if !isempty(content)
+  dom = Gumbo.parsehtml(content)
+  extract_links(dom.root)
+end
+
+println(LINKS)
