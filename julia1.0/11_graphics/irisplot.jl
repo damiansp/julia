f3645c08dfe65b05f50b7a327db7b6bec3629a6e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-18 08:33:01 -0700

    add irisplot (broken)

---
+++ julia1.0/11_graphics/irisplot.jl
@@ -0,0 +1,11 @@
+using PyPlot
+using RDatasets
+using StatsPlots
+
+
+iris = dataset("datasets", "iris")
+@df iris scatter(:SepalLength,
+                 :SepalWidth,
+                 group=:Species,
+                 m=(0.5, [:+ :h :star7], 4),
+                 bg=RGB(1.0, 1.0, 1.0))
