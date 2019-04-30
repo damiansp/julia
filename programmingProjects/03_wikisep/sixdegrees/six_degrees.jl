279a484596d6b2121e0f1af2a6936000a7ced430
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-29 23:27:18 -0400

    debug and test

--- programmingProjects/03_wikisep/sixdegrees/six_degrees.jl
+++ programmingProjects/03_wikisep/sixdegrees/six_degrees.jl
@@ -2,7 +2,11 @@ using Pkg
 pkg"activate ."
 
 include("Wikipedia.jl")
+include("Gameplay.jl")
+using .Gameplay
 using .Wikipedia
 
 
-fetchrandom() |> articlelinks |> display
+for article in newgame(Gameplay.DIFFICULTY_HARD)
+  println(article[:title])
+end
