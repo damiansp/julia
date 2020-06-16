0318e3940b089e94a3b1f721966e401f440818dc
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-06-16 08:12:02 -0700

    spacewar++; DF/functions++

--- designpatterns/3_functions_and_interfaces/spacewar.jl
+++ designpatterns/3_functions_and_interfaces/spacewar.jl
@@ -49,3 +49,50 @@ function makeasteroids(n::Int, posrange=0:200, sizerange=10:30)
 					 Size(szrand(), szrand()))
 		for i in 1:n]
 end
+
+makeasteroids(5)
+makeasteroids(5, 1:10)
+
+
+function makeasteroids2(n::Int; posrange=0:200, sizerange=10:30) # <- note ";"
+	posrand() = rand(posrange)
+	szrand() = rand(sizerange)
+	return [
+		Widget("Asteroid #$i", 
+					 Position(posrand(), posrand()), 
+					 Size(szrand(), szrand()))
+		for i in 1:n]
+end
+	
+asteroids = makeasteroids2(5)
+makeasteroids2(5, posrange=10:20) # keyword arg must be named
+
+
+function makeasteroids3(; n::Int, posrange=0:200, sizerange=10:30) # <- note ";"
+	posrand() = rand(posrange)
+	szrand() = rand(sizerange)
+	return [
+		Widget("Asteroid #$i", 
+					 Position(posrand(), posrand()), 
+					 Size(szrand(), szrand()))
+		for i in 1:n]
+end
+
+
+
+# varargs
+# shoot any number of targets
+function shoot(from::Widget, targets::Widget...)
+	println("Type of targets: ", typeof(targets))
+	for target in targets
+		println(from.name, " -> ", target.name)
+	end
+end
+
+spaceship = Widget("Spaceship", Position(0, 0), Size(30, 30))
+target1 = asteroids[1]
+target2 = asteroids[2]
+target3 = asteroids[3]
+
+shoot(spaceship, target1)
+shoot(spaceship, target1, target2, target3)
