9e2eb5c2076cd4b8c0c2b417fbec30a5eb7d0155
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-06-21 21:29:06 -0700

    dispatch++

--- designpatterns/3_functions_and_interfaces/dispatch.jl
+++ designpatterns/3_functions_and_interfaces/dispatch.jl
@@ -45,3 +45,54 @@ a2 = Asteroid(Position(0, 20), Size(20, 20))
 
 println(position(s1), size(s1), shape(s1))
 println(position(a1), size(a1), shape(a1))
+
+
+struct Rectangle
+	top::Int
+	left::Int
+	bottom::Int
+	right::Int
+	# Constructor returns upper-left and lower-right corners
+	Rectangle(p::Position, s::Size) = new(p.y + s.height, p.x, p.y, p.x + s.width)
+end
+
+# check if rectangles overlap
+function overlap(A::Rectangle, B::Rectangle)
+	return (A.left < B.right 
+					&& A.right > B.left 
+					&& A.top > B.bottom 
+					&& A.bottom < B.top)
+end
+
+function collide(A::Thing, B::Thing)
+	println("Checking collision of thing vs. thing")
+	rectA = Rectangle(position(A), size(A))
+	rectB = Rectangle(position(B), size(B))
+	return overlap(rectA, rectB)
+end
+
+collide(s1, s2)
+collide(a1, a2)
+collide(s1, a1)
+
+function collide(A::Spaceship, B::Spaceship)
+	println("Checking collision of spaceship vs. spaceship")
+	return true # just a test
+end
+
+collide(s1, s2)
+
+function collide(A::Asteroid, B::Thing)
+	println("Checking collision of asteroid vs. thing")
+	return true # test only
+end
+
+function collide(A::Thing, B::Asteroid)
+	println("Checking collision of thing vs. asteroid")
+	return false
+end
+
+collide(a1, s1)
+collide(s1, a1)
+
+#collide(a1, a2) # ambiguity
