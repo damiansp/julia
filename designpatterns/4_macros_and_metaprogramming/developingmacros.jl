f9cc0f184a9b0ee7fdde29fc08f47b3c19b33c24
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-26 14:54:38 -0700

    developingmacros++

--- designpatterns/4_macros_and_metaprogramming/developingmacros.jl
+++ designpatterns/4_macros_and_metaprogramming/developingmacros.jl
@@ -3,5 +3,33 @@ macro hello()
 	return :(for i in 1:3 println("hello world") end)
 end
 
-@hello() # or simplyt
+@hello() # or simply
 @hello
+
+
+macro hi(n)
+	return :(for i in 1:$n println("hi there!") end)
+end
+
+@hi(5)
+@hi 5
+
+a = 1
+b = "hello"
+c = :hey
+
+function showme(x)
+	@show x
+end
+
+showme(a) # x = 1
+showme(b) # x = "hello"
+showme(c) # x = :hey
+
+macro showit(x)
+	@show x
+end
+
+@showit a # x = :a
+@showit b # x = :b
+@showit c # x = :c
