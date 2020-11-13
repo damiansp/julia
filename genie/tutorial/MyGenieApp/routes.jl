d08af59e66290fff7574f2baf8064963c7d06d99
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-13 10:56:17 -0800

    Add form and param handling

--- genie/tutorial/MyGenieApp/routes.jl
+++ genie/tutorial/MyGenieApp/routes.jl
@@ -1,9 +1,27 @@
-using Genie.Router
+using Genie.Renderer.Html, Genie.Requests, Genie.Router
+
+form = """
+<form action="/" method="POST" enctype="multipart/form-data">
+  <input type="text" name="name" value="" placeholder="Name?" />
+  <input type="submit" value="Greet" />
+</form>
+"""
 
 route("/") do
-  serve_static_file("welcome.html")
+  #serve_static_file("welcome.html")
+  html(form)
 end
 
+route("/", method=POST) do
+  "Hello there, $(postpayload(:name, "Pardner"))"
+end
+
+
 route("/hello") do
-  "Welcome to Genie!"
+  # without using Requests:
+  # name = haskey(@params, :name) ? @params(:name) : "Pardner"
+  #"Welcome to Genie, $(name)!"
+
+  # with Requests:
+  "Well now, welcome to Genie, $(getpayload(:name, "Pardner"))"
 end
