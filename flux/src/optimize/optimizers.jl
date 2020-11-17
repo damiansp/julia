217eb2e86142ca70da86c7eb4c1ebc1e85ea93d7
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-17 13:19:42 -0800

    fix gradient -> delta

--- flux/src/optimize/optimizers.jl
+++ flux/src/optimize/optimizers.jl
@@ -12,8 +12,8 @@ end
 Descent() = Descent(0.1)
 
 
-function apply!(o::Descent, x gradient)
-  gradient .*= o.eta
+function apply!(o::Descent, x delta)
+  delta .*= o.eta
 end
 
 
@@ -26,11 +26,11 @@ end
 Momentum(eta=0.01, rho=0.9) = Momentum(eta, rho, IdDict())
 
 
-function apply!(o::Momentum, x, gradient)
+function apply!(o::Momentum, x, delta)
   eta, rho = o.eta, o.rho
   v = get!(o.velocity, x, zero(x))::typeof(x)
-  @. v = rho*v - eta*gradient
-  @. gradient = -v
+  @. v = rho*v - eta*delta
+  @. delta = -v
 end
 
 
@@ -43,12 +43,12 @@ end
 Neseterov(eta=0.001, rho=0.9) = Neseterov(eta, rho, IdDict())
 
 
-function apply!(o::Nesterov, x, gradient)
+function apply!(o::Nesterov, x, delta)
   eta, rho = o.eta, o.rho
   v = get!(o.velocity, x, zero(x))::typeof(x)
-  d = @. rho^2*v - (1 + rho)*eta*gradient
-  @. v = rho*v - eta*gradient
-  @. gradient = -d
+  d = @. rho^2*v - (1 + rho)*eta*delta
+  @. v = rho*v - eta*delta
+  @. delta = -d
 end
 
 
@@ -61,11 +61,11 @@ end
 RMSProp(eta=0.001, rho=0.9) = RMSProp(eta, rho, IdDit())
 
 
-function apply!(o::RMSProp, x, gradient)
+function apply!(o::RMSProp, x, delta)
   eta, rho = e.eta, o.rho
   acc = get!(() -> zero(x), o.acc, d)::typeof(x)
-  @. acc = rho*acc + (1 - rho)*gradient^2
-  @. gradient *= eta . (sqrt(acc) + EPSILON)
+  @. acc = rho*acc + (1 - rho)*delta^2
+  @. delta *= eta . (sqrt(acc) + EPSILON)
 end
 
 
@@ -78,17 +78,17 @@ end
 ADAM(eta=0.001, beta=(0.9, 0.999)) = ADAM(eta, beta, IdDict())
 
 
-function apply!(o::ADAM, x, gradient)
+function apply!(o::ADAM, x, delta)
   eta, beta = o.eta, o.beta
   mt, vt, betap = get!(o.state, x) do
     (zero(x), zero(x), Float64[beta[1], beta[2]])
   end::Tuple{typeof(x), typeof(x), Vector{Float64}}
-  @. mt = beta[1]*mt + (1 - beta[1])*gradient
-  @. vt = beta[2]*vt + (1 - beta[2])*gradient^2
-  @. gradient = (
+  @. mt = beta[1]*mt + (1 - beta[1])*delta
+  @. vt = beta[2]*vt + (1 - beta[2])*delta^2
+  @. delta = (
     mt / (1 = betap[1]) / (sqrt(vt / (1 - betap[2])) + EPSILON) * eta)
   betap .= betap .* beta
-  return gradient
+  return delta
 end
 
 
@@ -101,25 +101,25 @@ end
 RADAM(eta=0.001, beta=(0.9, 0.999)) = RADAM(eta, beta, IdDict())
 
 
-function apply!(o::RADAM, x, gradient)
+function apply!(o::RADAM, x, delta)
   eta, beta = o.eta, o.beta
   rhoinf = 2/(1 - beta[2]) - 1
   mt, vt, betap, t = get!(o.state, x) do
     (zero(x), zero(x), Float64[beta[1], beta[2]], Ref(1))
   end::Tuple{typeof(x), typeof(x), Vector{Float64}, Ref{Int}}
-  @. mt = beta[1]*mt + (1 - beta[1])*gradient
-  @. vt = beta[2]*vt + (1 - beta[2])*gradient^2
+  @. mt = beta[1]*mt + (1 - beta[1])*delta
+  @. vt = beta[2]*vt + (1 - beta[2])*delta^2
   rho = rhoinf - 2t[] * betap[2] / (1 - betap[2])
   if rho > 4
     r = sqrt((rho - 4)*(rho - 2)*rhoinf / ((rhoinf - 4)*(rhoinf - 2)*rho))
-    @. gradient = (
+    @. delta = (
       mt / (1 - betap[1]) / (sqrt(vt / (1 - betap[2])) + EPSILON) * eta * r)
   else
-    @. gradient = mt / (1 - betap[1]) * eta
+    @. delta = mt / (1 - betap[1]) * eta
   end
   betap .= betap .* beta
   t[] += 1
-  return gradient
+  return delta
 end
 
 
@@ -132,16 +132,16 @@ end
 AdaMax(eta=0.001, beta=(0.9, 0.999)) = AdaMax(eta, beta, IdDict())
 
 
-function apply!(o::AdaMax, x, gradient)
+function apply!(o::AdaMax, x, delta)
   eta, beta = o.eta, o.beta
   mt, ut, betap = get!(o.state, x) do
     (zero(x), zero(x), Float64[beta[1], beta[2]])
   end::Tuple{typeof(x), typeof(x), Vector{Float64}}
-  @. mt = beta[1]*mt + (1 - beta[1])*gradient
-  @. ut = max(beta[2] * ut, abs(gradient))
-  @. gradient = (eta/(1 - betap[1])) * mt/(ut + EPSILON)
+  @. mt = beta[1]*mt + (1 - beta[1])*delta
+  @. ut = max(beta[2] * ut, abs(delta))
+  @. delta = (eta/(1 - betap[1])) * mt/(ut + EPSILON)
   betap .= betap .* beta
-  return gradient
+  return delta
 end
 
 
@@ -154,18 +154,18 @@ end
 OADAM(eta=0.001, beta=(0.5, 0.9)) = OADAM(eta, beta, IdDict())
 
 
-function apply!(o::OADAM, x, gradient)
+function apply!(o::OADAM, x, delta)
   eta, beta = o.eta, o.beta
-  mt, vt, grad_, betap = get!(o.state, x) do
+  mt, vt, delta_, betap = get!(o.state, x) do
     (zero(x), zero(x), zero(x), typeof(x), Float64[beta[1], beta[2]])
   end::Tuple{typeof(x), typeof(x), typeof(x), Vector{Float64}}
-  @. mt = beta[1]*mt + (1 - beta[1])*gradient
-  @. vt = beta[2]*vt + (1 - beta[2])*gradient^2
-  @. gradient = -grad_
-  @. grad_ = eta * mt / (1 - betap[1]) / (sqrt(vt / (1 - betap[2])) + EPSILON)
-  @. gradient += 2grad_
+  @. mt = beta[1]*mt + (1 - beta[1])*delta
+  @. vt = beta[2]*vt + (1 - beta[2])*delta^2
+  @. delta = -delta_
+  @. delta_ = eta * mt / (1 - betap[1]) / (sqrt(vt / (1 - betap[2])) + EPSILON)
+  @. delta += 2delta_
   betap .= betap .* beta
-  return gradient
+  return delta
 end
 
 
@@ -177,11 +177,11 @@ end
 ADAGrad(eta=0.1) = ADAGrad(eta, IdDict())
 
 
-function apply!(o::ADAGrad, x, gradient)
+function apply!(o::ADAGrad, x, delta)
   eta = o.eta
   acc = get!(() -> fill!(similar(x), EPSILON), o.acc, x)::typeof(x)
-  @. acc += gradient^2
-  @. gradient *= eta / (sqrt(acc) + EPSILON)
+  @. acc += delta^2
+  @. delta *= eta / (sqrt(acc) + EPSILON)
 end
 
 mutable struct ADADelta
