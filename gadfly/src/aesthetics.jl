02f1fbc301a683d24812bdd7ef56e19104744d19
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-19 08:38:02 -0800

    aesthetics++

--- gadfly/src/aesthetics.jl
+++ gadfly/src/aesthetics.jl
@@ -92,3 +92,54 @@ const NumericalOrCategoricalAesthetic = Union{CategoricalAesthetic,
   pad_categorical_x, Union{Missing, Bool}, missing
   pad_categorical_y, Union{Missing, Bool}, missing
 end
+
+# Calculating fieldnames at runtime is expensive
+const valid_aesthetics = fieldnames(Aesthetics)
+
+
+function show(io::IO, data::Aesthetics)
+  maxlen = 0
+  print(io, "Aesthetics(")
+  for name in valid_aesthetics
+    val = getfield(data, name)
+    if !ismissing(val) && val != nothing
+      print(io, "\n ", string(name), "=")
+      show(io, getfield(data, name))
+    end
+  end
+  print(io, "\n)\n")
+end
+
+# Aliased aesthetic names
+const aesthetic_aliases = Dict{Symbol, Symbol}(
+  :colour => :color,
+  :x_min => :xmin,
+  :x_max => :xmax,
+  :y_min => :ymin,
+  :y_max => :ymax,
+  :x_group => :xgroup,
+  :y_group => :ygroup,
+  :x_viewmin => :xviewmin,
+  :x_viewmax => :xviewmax,
+  :y_viewmin => :yviewmin,
+  :y_viewmax => :yviewmax,
+  :x_group_label => :xgroup_label,
+  :y_group_label => :ygroup_label,
+  :x_tick => :xtick,
+  :y_tick => :ytick,
+  :x_grid => :xgrid,
+  :y_grid => :ygrid)
+
+# Index as if this were a DataFrame
+getindex(aes::Aesthetics, i::Integer, j::AbstractString) = getfield(
+  aes, Symbol(j)
+)[i]
+
+
+# Return the set of non-nothing variables
+function defined_aesthetics(aes::Aesthetics)
+  vars = Set{Symbol}()
+  for name in valid_aesthetics
+    getfield(aes, name) === nothing || push!(vars, name)
+  end
+end
