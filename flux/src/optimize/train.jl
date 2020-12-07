baee53519edd4f69bc805338b664feb3c597fa86
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-12-07 08:50:48 -0800

    train++

--- flux/src/optimize/train.jl
+++ flux/src/optimize/train.jl
@@ -50,3 +50,53 @@ function skip()
   throw(SkipException())
 end
 
+
+struct StopException <: Exception end
+
+
+"""
+  stop()
+Call `Flux.stop()` in a callback to indicate when a callback condition is met.
+This will trigger the train loop to stop and exit.
+"""
+function stop()
+  throw(StopException())
+end
+
+batchmemaybe(x) = tuple(x)
+batchmemaybe(x::Tuple) = x
+
+
+"""
+  train!(loss, params, data, opt; cb)
+For each datapoint `d` in `data`, compute the gradient of `loss` with respect to
+`params` through propagation and call the optimizer `opt`.
+If `d` is a tuple of args to `loss` call `loss(d...)`, else `loss(d)`.
+A callback is given with the keyword `cb`. For example, this will print
+"training" every 10s:
+  train!(loss, params data, opt, cb=throttle(() -> println("training), 10))
+The callback can cal [`Flux.stop`](@ref) to interrupt the training loop.
+Multiple optimizers and callbacks can be passed to `opt` and `cb` as arrays.
+"""
+function train!(loss, ps, data, opt; cb=() -> ())
+  ps = Prams(ps)
+  cb = runall(cb)
+  @progress for d in data
+    try
+      gs = gradient(ps) do
+        loss(batchmemaybe(d)...)
+      end
+      update!(opt, ps, gs)
+      cb()
+    catch ex
+      if ex isa StopException
+        break
+      elseif ex isa SkipException
+        continue
+      else
+        rethrow(ex)
+      end
+    end
+  end
+end
+
